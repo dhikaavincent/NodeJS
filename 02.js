12a467d41d7ac035f8e7bb6b1cc9fe4f4822adc9
dhikaavincent <dhika.kusuma.ardyaksa@gmail.com>
2022-09-25 21:08:31 +0700

    Task Day 4

---
+++ 02.js
@@ -0,0 +1,5 @@
+var angka= 1212821821213;
+    if (angka%2==0)
+        console.log("Genap");
+    else
+        console.log("Ganjil");
---
+++ 04.js
@@ -0,0 +1,13 @@
+let umur = 76;
+let result = ""
+
+if (umur >= 21){
+    result = "Dewasa"
+    } else if (umur >= 13){
+    result = "Remaja"
+    } else if (umur >=9){
+    result = "Bimbingan orang tua"
+    } else if (umur < 9){
+    result = "Semua usia"
+    }
+console.log(result);
